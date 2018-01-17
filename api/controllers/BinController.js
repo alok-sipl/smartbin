8a51d53b7bdde6c7aa3a14fd136f4f5acb54e115
alok.bichhwe <alok.bichhwe@systematixindia.com>
2018-01-17 11:31:57 +0530

    update ordering of ward

--- api/controllers/BinController.js
+++ api/controllers/BinController.js
@@ -74,46 +74,46 @@
     var ref = db.ref("bins");
     ref.once('value', function (snap) {
       var tempBinRecords = [];
-       _.map(snap.val(),function (val, bin_key){
-            val.bin_key = bin_key;
-            tempBinRecords.push(val)
-        });
-     if(Object.keys(snap.val()).length){
-      var i = 0;
-      async.forEach(tempBinRecords, function (childSnap, callback){
-        updateBin = {};  
-           var ref = db.ref("areas/" + childSnap.ward_id + "/" + childSnap.area_id);
-           ref.once("value", function (snapshot) {
+      _.map(snap.val(),function (val, bin_key){
+        val.bin_key = bin_key;
+        tempBinRecords.push(val)
+      });
+      if(Object.keys(snap.val()).length){
+        var i = 0;
+        async.forEach(tempBinRecords, function (childSnap, callback){
+          updateBin = {};  
+          var ref = db.ref("areas/" + childSnap.ward_id + "/" + childSnap.area_id);
+          ref.once("value", function (snapshot) {
             var area = snapshot.val();
-        var ref = db.ref("circlewards/" + childSnap.circle_id + "/" + childSnap.ward_id);
-       ref.once("value", function (snapshotWards) {
-        var ward = snapshotWards.val();
-        bindetails = childSnap;
-          updateBin = bindetails; 
-          updateBin.latitude = parseFloat(updateBin.latitude);
-          updateBin.longitude = parseFloat(updateBin.longitude);
-          if(ward){
-            updateBin.ward_name = ward.name;
-          }
-          if(area){
-            updateBin.area_name = area.name;
-       } 
-      bins.push(updateBin);
-      i++;
-    callback();
-      if(i  == Object.keys(snap.val()).length){
-       return res.json({'rows':bins});
+            var ref = db.ref("circlewards/" + childSnap.circle_id + "/" + childSnap.ward_id);
+            ref.once("value", function (snapshotWards) {
+              var ward = snapshotWards.val();
+              bindetails = childSnap;
+              updateBin = bindetails; 
+              updateBin.latitude = parseFloat(updateBin.latitude);
+              updateBin.longitude = parseFloat(updateBin.longitude);
+              if(ward){
+                updateBin.ward_name = ward.name;
+              }
+              if(area){
+                updateBin.area_name = area.name;
+              } 
+              bins.push(updateBin);
+              i++;
+              callback();
+              if(i  == Object.keys(snap.val()).length){
+               return res.json({'rows':bins});
+             }
+           });
+          });
+        });
+      }else{
+        bins ={};
+        return bins;
       }
-    });
-});
-});
-}else{
-  bins ={};
-  return bins;
-}
 
     });
-},
+  },
 
   /*
      * Name: add
@@ -198,9 +198,9 @@
                  /* }, function (errorObject) {
                     return res.serverError(errorObject.code);
                   });*/
-              }, function (errorObject) {
-                return res.serverError(errorObject.code);
-              });
+                }, function (errorObject) {
+                  return res.serverError(errorObject.code);
+                });
               } else {
                 if (uploadedFiles.length === 0) {
                   errors['image'] = {message: Bin.message.bin_image_required}
@@ -225,9 +225,9 @@
                   /*  }, function (errorObject) {
                       return res.serverError(errorObject.code);
                     });*/
-                }, function (errorObject) {
-                  return res.serverError(errorObject.code);
-                });
+                  }, function (errorObject) {
+                    return res.serverError(errorObject.code);
+                  });
                 } else if (allowExts.indexOf(uploadedFiles[0].type) == -1) {
                   errors['image'] = {message: Bin.message.invalid_file}
                   /* country listing*/
@@ -252,9 +252,9 @@
                 /*    }, function (errorObject) {
                       return res.serverError(errorObject.code);
                     });*/
-                }, function (errorObject) {
-                  return res.serverError(errorObject.code);
-                });
+                  }, function (errorObject) {
+                    return res.serverError(errorObject.code);
+                  });
                 } else if (bindata) {
                   req.flash('flashMessage', '<div class="alert alert-danger">' + req.param('id') + sails.config.flash.bin_id_already_exist + '</div>');
                   return res.redirect(sails.config.base_url + 'bin/add');
@@ -318,9 +318,9 @@
    /* }, function (errorObject) {
       return res.serverError(errorObject.code);
     });*/
-}, function (errorObject) {
-  return res.serverError(errorObject.code);
-});
+  }, function (errorObject) {
+    return res.serverError(errorObject.code);
+  });
 }
 },
 
@@ -391,21 +391,21 @@
                         });
                       });
                     });
-/* Get Areas */
-});
-/* Get Cities */
-});
-});
-/* Get States */
-}, function (errorObject) {
-  return res.serverError(errorObject.code);
-});
-}, function (errorObject) {
-  return res.serverError(errorObject.code);
-});
-} else {
-  var allowExts = ['image/png', 'image/jpg', 'image/jpeg'];
-  req.file('image').upload({
+                    /* Get Areas */
+                  });
+                  /* Get Cities */
+                });
+              });
+              /* Get States */
+            }, function (errorObject) {
+              return res.serverError(errorObject.code);
+            });
+          }, function (errorObject) {
+            return res.serverError(errorObject.code);
+          });
+        } else {
+          var allowExts = ['image/png', 'image/jpg', 'image/jpeg'];
+          req.file('image').upload({
           // don't allow the total upload size to exceed ~4MB
           maxBytes: sails.config.length.max_file_upload
         }, function whenDone(err, uploadedFiles) {
@@ -458,110 +458,110 @@
                           });
                         });
                       });
-/* Get Areas */
-});
-/* Get Cities */
-});
-});
-/* Get States */
-}, function (errorObject) {
-  return res.serverError(errorObject.code);
-});
-}, function (errorObject) {
-  return res.serverError(errorObject.code);
-});
-} else {
-  if ((Object.keys(uploadedFiles).length) && (allowExts.indexOf(uploadedFiles[0].type) == -1)) {
-    errors['image'] = {message: Bin.message.invalid_file}
-
-    var ref = db.ref("countries");
-    ref.once("value", function (snapshot) {
-
-      var countries = snapshot.val();
-
-      /* bin detail */
-      var ref = db.ref("bins/" + req.params.id);
-      ref.once("value", function (snapshot) {
-        var bin = snapshot.val();
-        /* city name */
-        ref.once("value", function (snapshot) {
-
-          var refState = db.ref("states/" + bin.country_id);
-          refState.once("value", function (snapshotState) {
-            var states = snapshotState.val();
-
-            var refCity = db.ref("cities/" + bin.state_id);
-            refCity.once("value", function (snapshotCity) {
-              var cities = snapshotCity.val();
-
-              var refCircle = db.ref("circles/" + bin.city_id);
-              refCircle.once("value", function (snapshotCircle) {
-                var circles = snapshotCircle.val();
-
-                var refWards = db.ref("circlewards/" + bin.circle_id);
-                refWards.once("value", function (snapshotWards) {
-                  var wards = snapshotWards.val();
-
-                  var refAreas = db.ref("areas/" + bin.ward_id);
-                  refAreas.once("value", function (snapshotAreas) {
-                    var areas = snapshotAreas.val();
-
-                    return res.view('add-update-bin', {
-                      title: sails.config.title.edit_bin,
-                      'bin': bin,
-                      "countries": countries,
-                      "states": states,
-                      "cities": cities,
-                      'wards' : wards,
-                      'circles' : circles,
-                      'areas': areas,
-                      "errors": errors
+                      /* Get Areas */
                     });
+                    /* Get Cities */
                   });
                 });
+                /* Get States */
+              }, function (errorObject) {
+                return res.serverError(errorObject.code);
               });
-              /* Get Areas */
+            }, function (errorObject) {
+              return res.serverError(errorObject.code);
             });
-/* Get Cities */
-});
-});
-/* Get States */
-}, function (errorObject) {
-  return res.serverError(errorObject.code);
-});
-}, function (errorObject) {
-  return res.serverError(errorObject.code);
-});
+          } else {
+            if ((Object.keys(uploadedFiles).length) && (allowExts.indexOf(uploadedFiles[0].type) == -1)) {
+              errors['image'] = {message: Bin.message.invalid_file}
 
-} else {
-  var ref = db.ref();
-  db.ref('bins/' + req.params.id)
-  .update({
-    alert_level: req.param('alert_level'),
-    area_id: req.param('area'),
-    ward_id : req.param('ward'),
-    city_id: req.param('city'),
-    country_id: req.param('country'),
-    is_deleted: false,
-    latitude: req.param('latitude'),
-    location: req.param('location'),
-    longitude: req.param('longitude'),
-    modified_date: Date.now(),
-    circle_id : req.param('circle'),
-    name: req.param('name'),
-    state_id: req.param('state'),
-  })
-  .then(function (res) {
-    req.flash('flashMessage', '<div class="alert alert-success">' + sails.config.flash.bin_updated_success + '</div>');
-    return res.redirect(sails.config.base_url + 'bin');
-  })
-  .catch(function (err) {
-    req.flash('flashMessage', '<div class="alert alert-error">' + sails.config.flash.bin_add_error + '</div>');
-    return res.redirect(sails.config.base_url + 'bin/edit/' + req.params.id);
-  });
-}
-}
-});
+              var ref = db.ref("countries");
+              ref.once("value", function (snapshot) {
+
+                var countries = snapshot.val();
+
+                /* bin detail */
+                var ref = db.ref("bins/" + req.params.id);
+                ref.once("value", function (snapshot) {
+                  var bin = snapshot.val();
+                  /* city name */
+                  ref.once("value", function (snapshot) {
+
+                    var refState = db.ref("states/" + bin.country_id);
+                    refState.once("value", function (snapshotState) {
+                      var states = snapshotState.val();
+
+                      var refCity = db.ref("cities/" + bin.state_id);
+                      refCity.once("value", function (snapshotCity) {
+                        var cities = snapshotCity.val();
+
+                        var refCircle = db.ref("circles/" + bin.city_id);
+                        refCircle.once("value", function (snapshotCircle) {
+                          var circles = snapshotCircle.val();
+
+                          var refWards = db.ref("circlewards/" + bin.circle_id);
+                          refWards.once("value", function (snapshotWards) {
+                            var wards = snapshotWards.val();
+
+                            var refAreas = db.ref("areas/" + bin.ward_id);
+                            refAreas.once("value", function (snapshotAreas) {
+                              var areas = snapshotAreas.val();
+
+                              return res.view('add-update-bin', {
+                                title: sails.config.title.edit_bin,
+                                'bin': bin,
+                                "countries": countries,
+                                "states": states,
+                                "cities": cities,
+                                'wards' : wards,
+                                'circles' : circles,
+                                'areas': areas,
+                                "errors": errors
+                              });
+                            });
+                          });
+                        });
+                        /* Get Areas */
+                      });
+                      /* Get Cities */
+                    });
+                  });
+                  /* Get States */
+                }, function (errorObject) {
+                  return res.serverError(errorObject.code);
+                });
+              }, function (errorObject) {
+                return res.serverError(errorObject.code);
+              });
+
+            } else {
+              var ref = db.ref();
+              db.ref('bins/' + req.params.id)
+              .update({
+                alert_level: req.param('alert_level'),
+                area_id: req.param('area'),
+                ward_id : req.param('ward'),
+                city_id: req.param('city'),
+                country_id: req.param('country'),
+                is_deleted: false,
+                latitude: req.param('latitude'),
+                location: req.param('location'),
+                longitude: req.param('longitude'),
+                modified_date: Date.now(),
+                circle_id : req.param('circle'),
+                name: req.param('name'),
+                state_id: req.param('state'),
+              })
+              .then(function (res) {
+                req.flash('flashMessage', '<div class="alert alert-success">' + sails.config.flash.bin_updated_success + '</div>');
+                return res.redirect(sails.config.base_url + 'bin');
+              })
+              .catch(function (err) {
+                req.flash('flashMessage', '<div class="alert alert-error">' + sails.config.flash.bin_add_error + '</div>');
+                return res.redirect(sails.config.base_url + 'bin/edit/' + req.params.id);
+              });
+            }
+          }
+        });
 }
 } else {
   /* country listing*/
@@ -613,18 +613,18 @@
                 });
               });
             });
-/* Get Areas */
-});
-/* Get Cities */
-});
-});
-/* Get States */
-}, function (errorObject) {
-  return res.serverError(errorObject.code);
-});
-}, function (errorObject) {
-  return res.serverError(errorObject.code);
-});
+            /* Get Areas */
+          });
+          /* Get Cities */
+        });
+      });
+      /* Get States */
+    }, function (errorObject) {
+      return res.serverError(errorObject.code);
+    });
+  }, function (errorObject) {
+    return res.serverError(errorObject.code);
+  });
 
 }
 },
@@ -665,32 +665,35 @@
      * @param  int  $id
      */
      search: function (req, res) {
-               var wards = [];
-        db.ref('/bins')
-        .once('value')
-        .then(function (snapshot) {
-          var bins = snapshot.val();
+       var wards = [];
+       db.ref('/bins')
+       .once('value')
+       .then(function (snapshot) {
+        var bins = snapshot.val();
 
-var ref = db.ref("circlewards");
-       ref.once("value", function (snapshotWards) {
+        var ref = db.ref("circlewards");
+        ref.once("value", function (snapshotWards) {
          _.map(snapshotWards.val(),function (val, bin_key){
-            _.map(val,function (val2, ward_key){
-              val2.id = ward_key;
+          _.map(val,function (val2, ward_key){
+            val2.id = ward_key;
             wards.push(val2)
-        });
-        });
-console.log(wards);
-          return res.view('search-bin', {
-            title: sails.config.title.supplier_list,
-            bins: bins,
-            wards: wards,
           });
-        }).catch(function (err) {
-          req.flash('flashMessage', '<div class="alert alert-error">' + sails.config.flash.something_went_wronge + '</div>');
-          return res.redirect('bin/search');
         });
-          });
-    },
+         wards.sort(function (a, b) {
+            return a.name - b.name;
+        })
+         console.log(wards);
+         return res.view('search-bin', {
+          title: sails.config.title.supplier_list,
+          bins: bins,
+          wards: wards,
+        });
+       }).catch(function (err) {
+        req.flash('flashMessage', '<div class="alert alert-error">' + sails.config.flash.something_went_wronge + '</div>');
+        return res.redirect('bin/search');
+      });
+     });
+     },
 
 
   /*
@@ -703,38 +706,38 @@ console.log(wards);
      filterbins: function (req, res) {
       var selected_ward = req.body.selected_ward;
 //console.log("selected_ward==",selected_ward);
-        if(!selected_ward)
-        {
+if(!selected_ward)
+{
          // console.log("search without ward11");
 
-          db.ref('/bins')
-          .once('value')
-          .then(function (snapshot) {
-            var bins = snapshot.val();
-            return res.json({'bins':bins});
-          }).catch(function (err) {
-            req.flash('flashMessage', '<div class="alert alert-error">' + sails.config.flash.something_went_wronge + '</div>');
-            return res.redirect('bin/search');
-          });
-        }
-        else
-        {
+         db.ref('/bins')
+         .once('value')
+         .then(function (snapshot) {
+          var bins = snapshot.val();
+          return res.json({'bins':bins});
+        }).catch(function (err) {
+          req.flash('flashMessage', '<div class="alert alert-error">' + sails.config.flash.something_went_wronge + '</div>');
+          return res.redirect('bin/search');
+        });
+      }
+      else
+      {
          // console.log("search with ward");
        //   console.log(selected_ward);
-          db.ref('bins').orderByChild("ward_id").equalTo(selected_ward).once('value')
-          .then(function (snapshot) {
+       db.ref('bins').orderByChild("ward_id").equalTo(selected_ward).once('value')
+       .then(function (snapshot) {
 
-            var bins = snapshot.val();
-            return res.json({'bins':bins});
-          }).catch(function (err) {
-            req.flash('flashMessage', '<div class="alert alert-error">' + sails.config.flash.something_went_wronge + '</div>');
-            return res.redirect('bin/search');
-          });
-        }
+        var bins = snapshot.val();
+        return res.json({'bins':bins});
+      }).catch(function (err) {
+        req.flash('flashMessage', '<div class="alert alert-error">' + sails.config.flash.something_went_wronge + '</div>');
+        return res.redirect('bin/search');
+      });
+    }
 
-     
-    },
-  };
+
+  },
+};
 
 /*
    * Name: getBinList
@@ -746,23 +749,23 @@ console.log(wards);
    function getBinsList(snap){
    // console.log(Object.keys(snap.val()).length);
 
-    if(Object.keys(snap).length){
-      var i = 0;
-      snap.forEach(function (childSnap) {
-        bindetails = childSnap.val();
+   if(Object.keys(snap).length){
+    var i = 0;
+    snap.forEach(function (childSnap) {
+      bindetails = childSnap.val();
         updateBin = {};    //    console.log("bindetails.city_id==",bindetails.ward_id);
 
         updateBin.bin_key =  childSnap.key;
         updateBin = bindetails;    //    console.log("bindetails.city_id==",bindetails.ward_id);
 
         bins.push(updateBin);
-       
+
 
       });
-       return bins;
-    }else{
-      bins ={};
-      return bins;
-    }
+    return bins;
+  }else{
+    bins ={};
+    return bins;
   }
+}
 
